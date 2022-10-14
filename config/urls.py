838987117f03f7d4ac8c9d4e732e46b6bc629bc9
KIMIRE <59990695+dlfpire@users.noreply.github.com>
2022-10-15 06:07:29 +0900

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+# 서버의 루트주소/polls 라는 주소에 polls 앱 url을 연결시킴
